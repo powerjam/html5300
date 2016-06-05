3c2f3042e95a12e4f56535b18e55cb1584f2a08a
jamie powers do <powerjam@uw.edu>
2016-06-05 15:11:54 -0700

    Adding final project

---
+++ project/js/components.js
@@ -0,0 +1,98 @@
+//Yelp widget
+
+(function() { var s = document.createElement("script");s.async = true;s.onload = s.onreadystatechange = function(){getYelpWidget("apropos-salon-and-barber-seattle-2","300","RED","n","n","1");};s.src='http://chrisawren.com/widgets/yelp/yelpv2.js' ;var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);})();
+
+
+//instafeedjs
+
+/*var feed = new Instafeed({
+    get: 'tagged',
+    tagName: 'beapropos',
+    clientId: 'YOUR_CLIENT_ID'
+});
+feed.run();*/
+
+      
+//Google Maps API   LatLang 47.6089476,-122.3288907
+
+var myCenter=new google.maps.LatLng(47.6089476,-122.3288907);
+
+function initialize()
+{
+var mapProp = {
+  center:myCenter,
+  zoom:15,
+  mapTypeId:google.maps.MapTypeId.ROADMAP
+  };
+
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker=new google.maps.Marker({
+  position:myCenter,
+  });
+
+marker.setMap(map);
+
+var infowindow = new google.maps.InfoWindow({
+  content:"<b>Apropos Salon &amp; Barber</b> is located<br> inside Sorrento Hotel"
+  });
+
+infowindow.open(map,marker);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+//Goole Maps API _ Responsive
+/*$( document ).ready( function() {
+
+$('body').noisy({
+    intensity: 0.2,
+    size: 200,
+    opacity: 0.28,
+    randomColors: false, // true by default
+    color: '#000000'
+});
+  
+	//Google Maps JS
+	//Set Map
+	function initialize() {
+			var myLatlng = new google.maps.LatLng(53.3333,-3.08333);
+			var imagePath = 'http://m.schuepfen.ch/icons/helveticons/black/60/Pin-location.png'
+			var mapOptions = {
+				zoom: 11,
+				center: myLatlng,
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			}
+
+		var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+		//Callout Content
+		var contentString = 'Some address here..';
+		//Set window width + content
+		var infowindow = new google.maps.InfoWindow({
+			content: contentString,
+			maxWidth: 500
+		});
+
+		//Add Marker
+		var marker = new google.maps.Marker({
+			position: myLatlng,
+			map: map,
+			icon: imagePath,
+			title: 'image title'
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+			infowindow.open(map,marker);
+		});
+
+		//Resize Function
+		google.maps.event.addDomListener(window, "resize", function() {
+			var center = map.getCenter();
+			google.maps.event.trigger(map, "resize");
+			map.setCenter(center);
+		});
+	}
+
+	google.maps.event.addDomListener(window, 'load', initialize);
+
+});*/
